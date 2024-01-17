6198aae8ba2fbfa6f7691ac689c2e703afcf9274
carlsonz16 <78499570+carlsonz16@users.noreply.github.com>
2024-01-17 16:42:23 -0500

    Create visualPageCtrl.js

---
+++ visualPageCtrl.js
@@ -0,0 +1,12 @@
+// JavaScript source code
+
+function openNav() {
+	document.getElementById("mySidebar").style.width = "250px";
+	document.getElementById("main").style.marginLeft = "250px";
+	document.getElementById("openButton").style.display = "none";
+}
+
+function closeNav() {
+	document.getElementById("mySidebar").style.width = "0";
+	document.getElementById("main").style.marginLeft = "0";
+	document.getElementById("openButton").style.display = "inline-block";
