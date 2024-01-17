3d9c89f608cff83d84c9e0da73e614bcf7e124bf
carlsonz16 <78499570+carlsonz16@users.noreply.github.com>
2024-01-17 16:31:06 -0500

    Add files via upload

---
+++ resumePageCtrl.js
@@ -0,0 +1,17 @@
+// JavaScript source code
+
+function openNav() {
+	document.getElementById("mySidebar").style.width = "250px";
+	document.getElementById("main").style.marginLeft = "250px";
+	document.getElementById("openButton").style.display = "none";
+}
+
+function closeNav() {
+	document.getElementById("mySidebar").style.width = "0";
+	document.getElementById("main").style.marginLeft = "0";
+	document.getElementById("openButton").style.display = "inline-block";
+}
+
+function Download(url) {
+	document.getElementById('resume').src = url;
+};
